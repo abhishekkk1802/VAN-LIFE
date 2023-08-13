8296ecb95a8816232f0dbbab191193845e5b45de
abhishek gangwar <gangwargangwar335@gmail.com>
2023-08-14 00:38:50 +0530

    added  host routes

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <footer>
+            @ 2023 #VANLIFE
+        </footer>
+    )
+}
---
+++ pages/Host/Reviews.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Reviews(){
+    return (
+        <h1>host review page</h1>
+    )
+}
